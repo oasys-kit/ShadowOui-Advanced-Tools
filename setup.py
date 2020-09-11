c9ec881c03158d875b563199feb835978a0e64ca
lucarebuffi <lrebuffi@anl.gov>
2020-09-11 10:34:03 -0500

    New Developments

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-ShadowOui-Advanced-Tools'
-VERSION = '1.0.31'
+VERSION = '1.0.32'
 ISRELEASED = True
 
 DESCRIPTION = 'ShadowOui advanced simulation tools'
